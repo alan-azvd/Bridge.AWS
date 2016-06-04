dc4ada72204654854f5f211767ad3a837acd2f53
Alan Azevedo <alan.azvd@gmail.com>
2016-06-04 15:29:10 -0300

    appveyor.yml configuration

--- pack.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Setting .nuspec version tag to $env:APPVEYOR_BUILD_VERSION"
-
-$content = (Get-Content src/Bridge.AWS.nuspec) 
-$content = $content -replace '\$version\$',$env:APPVEYOR_BUILD_VERSION
-
-$content | Out-File src/Bridge.AWS.nuspec
-
-nuget pack src/Bridge.AWS.csproj
